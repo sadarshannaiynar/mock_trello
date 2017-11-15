7a2ed621a445cb74b660903ee82e0e3d42e45b4a
Adarsh <adarshan_naiynar@icloud.com>
2017-11-15 11:19:07 +0530

    Added image loading

--- src/server/index.js
+++ src/server/index.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 const publicPath = path.resolve(__dirname, 'dist/public/');
+const iconPath = path.resolve(__dirname, 'icons');
 
 app.use(requestId({ paramName: 'requestId' }));
 app.use(logger.winston);
@@ -26,6 +27,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use('/public', express.static(publicPath, { redirect: false }));
+app.use('/icons', express.static(iconPath, { redirect: false }));
 
 routes.bind(app);
 
